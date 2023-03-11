d4c7c99c01da0f0034f65406bde1b052b8f3d62e
Sivachandran Paramasivam <sivachandran.paramasivam@platform.sh>
2023-03-11 11:36:06 +0530

    Fix absolute paths

--- server.py
+++ server.py
@@ -116,6 +116,7 @@ async def new_client_connected(client_scoket, path):
                     other_player = game.players[0] if client_scoket == game.players[1] else game.players[1]
                     await send_message('Lose', [client_scoket])
                     await send_message('Win', [other_player])
+
 async def start_server():
     print('SERVER STARTED')
     await websockets.serve(new_client_connected, 'localhost', 12345)
